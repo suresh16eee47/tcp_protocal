1d2ff53ce7701ab06778c9f3127ee1ec375bf541
suresh16eee47 <79818150+suresh16eee47@users.noreply.github.com>
2022-06-23 09:02:43 +0530

    Update workaround.kt

--- workaround.kt
+++ workaround.kt
@@ -1,3 +1,13 @@
-val str = 0xaa55.toString(16)
-val bytes = str.toByteArray()
-for (byte in bytes) println(byte)
+fun main() {
+    var data = 170000000.toString(16).toByteArray()
+    var packet_Name = "transmitter frequency configuration"
+    var packet_Header  = 0xaa55.toByte()
+    var packet_ID  = ""
+    var packet_Response = "0000"
+    var data_Length = "0008"
+    var packet_data_field = data
+    if (packet_Name == "transmitter frequency configuration"){
+        packet_ID = "5200"
+    }
+    println("packet header :${packet_Header}")
+}
